bef23934d626f81293b147aae9841ad0eb549239
Anh Vu <vu.haianh291@gmail.com>
2018-04-18 16:11:01 +0300

    Adding test

--- server/api/controllers/photoController.js
+++ server/api/controllers/photoController.js
@@ -1,6 +1,8 @@
 exports.upload = (req, res) => {
+    console.log('UPLOAD HERE');
+    console.log(req.file);
   if(!req.file) {
       return res.status(400).send();
   }
-  return res.status(200).send({message: 'Success'})
+  return res.status(200).send({filename: req.file.filename})
 };
