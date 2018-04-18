905169ca7919ef2dd527393cdfa4914b06888ed1
Anh Vu <vu.haianh291@gmail.com>
2018-04-18 15:39:33 +0300

    Fixing saving folder location

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, '/uploads/photos')
+        cb(null, 'uploads/photos')
     },
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + '.' + getExtension(file))
