7ce00a401491dbfab43f159ef2bd138243e8c488
Anh Vu <vu.haianh291@gmail.com>
2018-04-18 22:34:49 +0300

    Fixing url path

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'public/photos')
+        cb(null, '/public/photos')
     },
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + '.' + getExtension(file))
